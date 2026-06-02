15f624446d2523098f67eb1c10a6c167822ad94c
Kristal Pithwa <crystalpithwa@gmail.com>
2026-06-02 18:56:34 +0530

    feat: initialize root layout with AuthProvider and AppRouter components

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,5 @@
 import { AuthProvider } from "../context/auth";
-import { AppRouter } from "./Router";
+import { AppRouter } from "../components/Router";
 
 export default function RootLayout() {
   return (
